3f7a58d4ad47dbfe4964daf6c2bd854c06c05b9c
AbudeyeW <AbudeyeW@gmail.com>
2021-02-14 20:01:44 -0600

    Updated Data in Job

---
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -0,0 +1,41 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+public class Job {
+
+    private int id;
+    private static int nextId = 1;
+
+    private String name;
+    private Employer employer;
+    private Location location;
+    private PositionType positionType;
+    private CoreCompetency coreCompetency;
+
+    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
+    //  other five fields. The second constructor should also call the first in order to initialize
+    //  the 'id' field.
+
+
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer,Location location,PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer=employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+
+    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
+    //  match.
+
+    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+    //  and id.
+
+}
