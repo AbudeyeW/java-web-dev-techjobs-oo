6f2aa8e1570d03e550bf7f6ef4c87e263079dfcb
AbudeyeW <AbudeyeW@gmail.com>
2021-02-14 20:32:56 -0600

    Check ID and difference of 1 for JobTest

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class JobTest {
     Job job1;
     Job job2;
+    int delta = 1;
     @Before
     public void CreateJobObj(){
         job1 = new Job();
@@ -14,7 +15,9 @@ public class JobTest {
     }
     @Test
     public void testSettingJobId(){
-        assertEquals(job1,job2);
+        assertNotEquals(job1,job2);
+        //Test below shows there is a difference of 1
+        assertEquals(job1.hashCode(),job2.hashCode(),1);
     }
 
 }
