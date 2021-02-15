dedf21cd9886bac2dbc2dd3ae6ef1b17fe41de11
AbudeyeW <AbudeyeW@gmail.com>
2021-02-14 20:38:06 -0600

    TestJobConstructorSetsAllFields Test

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,17 +1,19 @@
 package org.launchcode.techjobs_oo.Tests;
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 import static org.junit.Assert.*;
 
 public class JobTest {
     Job job1;
     Job job2;
+    Job job3;
     int delta = 1;
     @Before
     public void CreateJobObj(){
         job1 = new Job();
         job2 = new Job();
+        job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
     @Test
     public void testSettingJobId(){
@@ -19,5 +21,12 @@ public class JobTest {
         //Test below shows there is a difference of 1
         assertEquals(job1.hashCode(),job2.hashCode(),1);
     }
-
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertTrue(job3.getName() instanceof String);
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertTrue(job3.getLocation() instanceof Location);
+        assertTrue(job3.getCoreCompetency() instanceof  CoreCompetency);
+        assertTrue(job3.getPositionType() instanceof  PositionType);
+    }
 }
